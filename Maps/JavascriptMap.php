12cda4676681f4b7952111a52587223c3f39e02f
Marcin Dryka <marcin@dryka.pl>
2011-10-15 16:57:17 +0200

    Refactoring. Separated file for static javascript. PHP renders only settings for GoogleMap

--- Maps/JavascriptMap.php
+++ Maps/JavascriptMap.php
@@ -1,20 +1,34 @@
 <?php
 
 namespace AntiMattr\GoogleBundle\Maps;
-
+use AntiMattr\GoogleBundle\Maps\Marker;
+/**
+ * JavascriptMap 
+ * 
+ * @uses AbstractMap
+ * @package GoogleBundle
+ * @author Marcin Dryka <marcin@dryka.pl> 
+ */
 class JavascriptMap extends AbstractMap
 {
-    const API_ENDPOINT = 'http://maps.googleapis.com/maps/api/js?';
+    const API_ENDPOINT = 'http://maps.googleapis.com/maps/api/js?'; //Google API endpoint
 
-    const TYPE_ROADMAP = 'ROADMAP';
+    // Google Maps supported types:
+    const TYPE_ROADMAP = 'ROADMAP';     
     const TYPE_SATELLITE = 'SATELLITE';
     const TYPE_HYBRID = 'HYBRID';
     const TYPE_TERRAIN = 'TERRAIN';
 
+    // Google Map Center
     protected $center = false;
+
+    // Default map type
     protected $type = self::TYPE_ROADMAP;
+
+    // Default do not fit to markers
     protected $fitToMarkers = false;
 
+    // Google Maps types array
     static protected $typeChoices = array(
         self::TYPE_ROADMAP => 'Road Map',
         self::TYPE_SATELLITE => 'Satellite',
@@ -22,9 +36,23 @@ class JavascriptMap extends AbstractMap
         self::TYPE_TERRAIN => 'Terrain',
     );
 
+    // Templating service
     protected $templating;
+
+    // Javascript click callback for map
     protected $clickCallback = null;
 
+    /**
+     * setType Set Google Map type 
+     * available options:
+     *   - JavascriptMap::TYPE_ROADMAP
+     *   - JavascriptMap::TYPE_SATELLITE
+     *   - JavascriptMap::TYPE_HYBRID
+     *   - JavascriptMap::TYPE_TERRAIN
+     * 
+     * @param string $type 
+     * @return void
+     */
     public function setType($type)
     {
         $type = (string) $type;
@@ -34,32 +62,44 @@ class JavascriptMap extends AbstractMap
         $this->type = $type;
     }
 
+    /**
+     * isTypeValid Validate Google Map type
+     * 
+     * @param mixed $type 
+     * @static
+     * @return bool
+     */
     static public function isTypeValid($type)
     {
         return array_key_exists($type, static::$typeChoices);
     }
 
+    /**
+     * getType get Google Map type
+     * 
+     * @return string
+     */
     public function getType()
     {
         return $this->type;
     }
 
-    public function setCenter($marker)
+    /**
+     * setCenter Set map center
+     * 
+     * @param mixed $marker 
+     * @return void
+     */
+    public function setCenter(Marker $marker)
     {
         $this->center = $marker;
-        $this->fitToMarkers = false;
     }
 
-    public function getFitToMarkers()
-    {
-        return $this->fitToMarkers;
-    }
-
-    public function setFitToMarkers($value)
-    {
-        $this->fitToMarkers = (bool) $value;
-    }
-
+    /**
+     * getCenter Get map center
+     * 
+     * @return void
+     */
     public function getCenter()
     {
         if (null !== $this->center)
@@ -74,11 +114,43 @@ class JavascriptMap extends AbstractMap
         return false;
     }
 
-    public function setTemplating($templating)
+    /**
+     * setFitToMarkers If set to true, Google Map is fit to markers (also calculate zoom)
+     * 
+     * @param bool $value 
+     * @return void
+     */
+    public function setFitToMarkers($value)
+    {
+        $this->fitToMarkers = (bool) $value;
+    }
+
+    /**
+     * getFitToMarkers Get fitToMarkers setting value
+     * 
+     * @return bool
+     */
+    public function getFitToMarkers()
+    {
+        return $this->fitToMarkers;
+    }
+
+    /**
+     * setTemplating Set twig templating service
+     * 
+     * @param mixed $templating 
+     * @return void
+     */
+    public function setTemplating(\Symfony\Bundle\TwigBundle\TwigEngine $templating)
     {
         $this->templating = $templating;
     }
 
+    /**
+     * renderContainer Render HTML container
+     * 
+     * @return string
+     */
     public function renderContainer()
     {
         return $this->templating->render('GoogleBundle:Maps:container.html.twig', array(
@@ -86,6 +158,11 @@ class JavascriptMap extends AbstractMap
         ));
     }
 
+    /**
+     * renderJavascript Render Javascripts settings
+     * 
+     * @return string
+     */
     public function renderJavascript()
     {
         return $this->templating->render('GoogleBundle:Maps:javascript.js.twig', array(
@@ -99,11 +176,22 @@ class JavascriptMap extends AbstractMap
         ));
     }
     
+    /**
+     * setClickCallback Set Javascript click callback (Javascript function)
+     * 
+     * @param string $callback Javascript function with one argument (coordinates). 
+     * @return void
+     */
     public function setClickCallback($callback)
     {
         $this->clickCallback = $callback;
     }
 
+    /**
+     * getGoogleMapLibrary Get HTML script tag with google map api library
+     * 
+     * @return string
+     */
     public function getGoogleMapLibrary()
     {
         $request = static::API_ENDPOINT;
@@ -113,6 +201,11 @@ class JavascriptMap extends AbstractMap
         return $content;
     }
 
+    /**
+     * render Google Map container and settings
+     * 
+     * @return string
+     */
     public function render()
     {
         //Get HTML container 
@@ -125,6 +218,4 @@ class JavascriptMap extends AbstractMap
 
         return $content;
     }
-
-    
 }
